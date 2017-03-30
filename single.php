3ffab75b0d13a52f1e82ba4944266da9e1cc7d81
Kate Datsenko <kate.datsenko@gmail.com>
2017-03-30 22:12:25 +0300

    Верстка блога Comments

--- single.php
+++ single.php
@@ -8,28 +8,30 @@
  */
 
 get_header(); ?>
+<div class="container-fluid">
+	<div class="row">
+		<div id="primary" class="content-area col-md-8">
+			<main id="main" class="site-main" role="main">
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', get_post_format() );
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
-		<?php
-		while ( have_posts() ) : the_post();
 
-			get_template_part( 'template-parts/content', get_post_format() );
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			endwhile; // End of the loop.
+			?>
+			</main><!-- #main -->
+		</div><!-- #primary -->
+		<div class="col-md-4">
+				<?php get_sidebar(); ?>
+		</div>
+	</div>
+</div>
 
 <?php
-get_sidebar();
 get_footer();
