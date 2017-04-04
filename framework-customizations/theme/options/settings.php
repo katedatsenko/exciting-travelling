4f11f4907ef94cf59ddf6a750621ce82a97d4ba0
Kate Datsenko <kate.datsenko@gmail.com>
2017-04-04 18:47:14 +0300

    Built-in Option Types: text, texarea, checkbox, checkboxes, radio

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -19,4 +19,78 @@ $options = array(
         'label' => __('Body Color', MY_THEME_TEXTDOMAIN),
         'value' => '#ADFF2F',
     ),
+    'option_regular_text' => array(
+        'type'  => 'text',
+        'value' => 'Default value',
+        'label' => __('Option Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Option Description', MY_THEME_TEXTDOMAIN),
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
+    ),
+    'option_regular_textarea' => array(
+        'type'  => 'textarea',
+        'value' => 'Default value',
+        'label' => __('Option Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Option Description', MY_THEME_TEXTDOMAIN),
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
+    ),
+    'option_single_checkbox' => array(
+        'type'  => 'checkbox',
+        'value' => true,
+        'label' => __('Option Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Option Description', MY_THEME_TEXTDOMAIN),
+        'text'  => __('Yes', MY_THEME_TEXTDOMAIN),
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'help'  => __('Some html that will appear in tip popup', MY_THEME_TEXTDOMAIN),
+    ),
+    'option_single_checkboxes' => array(
+        'type'  => 'checkboxes',
+        'value' => array(
+            'choice-1' => false,
+            'choice-2' => true,
+        ),
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'label' => __('Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Description', MY_THEME_TEXTDOMAIN),
+        'help'  => __('Help tip', MY_THEME_TEXTDOMAIN),
+        'choices' => array( // Примечание: избегайте использования ключей bool или int http://bit.ly/1cQgVzk
+            'choice-1' => __('Choice 1', MY_THEME_TEXTDOMAIN),
+            'choice-2' => __('Choice 2', MY_THEME_TEXTDOMAIN),
+            'choice-3' => __('Choice 3', MY_THEME_TEXTDOMAIN),
+        ),
+        // Display choices inline instead of list
+        'inline' => false,
+        //'inline' => true,
+    ),
+    'option_single_radio' => array(
+        'type'  => 'radio',
+        'value' => 'choice-3',
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'label' => __('Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Description', MY_THEME_TEXTDOMAIN),
+        'help'  => __('Help tip', MY_THEME_TEXTDOMAIN),
+        'choices' => array( // Примечание: избегайте использования ключей bool или int http://bit.ly/1cQgVzk
+            'choice-1' => __('Choice 1', MY_THEME_TEXTDOMAIN),
+            'choice-2' => __('Choice 2', MY_THEME_TEXTDOMAIN),
+            'choice-3' => __('Choice 3', MY_THEME_TEXTDOMAIN),
+        ),
+        // Display choices inline instead of list
+        'inline' => false,
+    ),
 );
