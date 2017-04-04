d1255db4c7ca04d83d13b195462dee5082ce1c1b
Kate Datsenko <kate.datsenko@gmail.com>
2017-04-04 19:11:09 +0300

    Built-in Option Types: bg-image

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -280,4 +280,34 @@ $options = array(
             ),
         'blank' => true, // (optional) Если true, изображения могут быть отменены
     ),
+    'option_background_image' => array(
+        'type'  => 'background-image',
+        'value' => 'bg-1',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+        'choices' => array(
+            'none' => array(
+                'icon' => get_template_directory_uri() . '/img/bg-3.png',
+                'css'  => array(
+                    'background-image' => 'none'
+                ),
+            ),
+            'bg-1' => array(
+                'icon'  => get_template_directory_uri() . '/img/bg-1.png',
+                'css'  => array(
+                    'background-image'  => 'url("' . get_template_directory_uri() . '/img/bg-1.png' . '")',
+                    'background-repeat' => 'repeat',
+                ),
+            ),
+            'bg-2' => array(
+                'icon' => get_template_directory_uri() . '/img/bg-2.png',
+                'css'  => array(
+                    'background-image'  => 'url("' . get_template_directory_uri() . '/img/bg-2.png' . '")',
+                    'background-repeat' => 'repeat-y'
+                ),
+            )
+        )
+    ),
 );
