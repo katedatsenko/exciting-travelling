a7799a6bd8ca43c5d346d7b97261dcc6d73ad7bf
Kate Datsenko <kate.datsenko@gmail.com>
2017-04-04 18:52:21 +0300

    Built-in Option Types: select, select multiple, milti-select, switch, color picker, GRBA color picker, gradient

--- framework-customizations/theme/options/settings.php
+++ framework-customizations/theme/options/settings.php
@@ -93,4 +93,154 @@ $options = array(
         // Display choices inline instead of list
         'inline' => false,
     ),
+    'option_single_select' => array(
+        'type'  => 'select',
+        'value' => 'choice-3',
+        'attr'  => array(
+            'class' => 'custom-class',
+            'data-foo' => 'bar'
+        ),
+        'label' => __('Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Description', MY_THEME_TEXTDOMAIN),
+        'help'  => __('Help tip', MY_THEME_TEXTDOMAIN),
+        'choices' => array(
+            '' => '---',
+            'choice-1' => __('Choice 1', MY_THEME_TEXTDOMAIN),
+            'choice-2' => array(
+                'text' => __('Choice 2', MY_THEME_TEXTDOMAIN),
+                'attr' => array('data-foo' => 'bar'),
+            ),
+            array( // optgroup
+                'attr'    => array('label' => __('Group 1', MY_THEME_TEXTDOMAIN)),
+                'choices' => array(
+                    'choice-3' => __('Choice 3', MY_THEME_TEXTDOMAIN),
+                    // ...
+                ),
+            ),
+        ),
+        /**
+         * Разрешить сохранение не существующих вариантов
+         * Полезно, когда вы используете select для его динамического заполнения из js
+         */
+        'no-validate' => false,
+    ),
+    'option_select_multiple' => array(
+        'type'  => 'select-multiple',
+        'value' => array( 'choice-1', 'choice-3' ),
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', MY_THEME_TEXTDOMAIN),
+        'desc'  => __('Description', MY_THEME_TEXTDOMAIN),
+        'help'  => __('Help tip', MY_THEME_TEXTDOMAIN),
+        'choices' => array(
+            '' => '---',
+            'choice-1' => __('Choice 1', MY_THEME_TEXTDOMAIN),
+            'choice-2' => array(
+                'text' => __('Choice 2', MY_THEME_TEXTDOMAIN),
+                'attr' => array('data-foo' => 'bar'),
+            ),
+            array( // optgroup
+                'attr'    => array('label' => __('Group 1', MY_THEME_TEXTDOMAIN)),
+                'choices' => array(
+                    'choice-3' => __('Choice 3', MY_THEME_TEXTDOMAIN),
+                    // ...
+                ),
+            ),
+        ),
+    ),
+    'option_select_multi_select' => array(
+        'type'  => 'multi-select',
+        'value' => array( 'choice-1', 'choice-3' ),
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+        /**
+         * Set population method
+         * Доступны: 'posts', 'taxonomy', 'users', 'array'
+         */
+        'population' => 'array',
+        /**
+         * Установить post types, taxonomies, user roles для поиска
+         *
+         * 'population' => 'posts'
+         * 'source' => 'page',
+         *
+         * 'population' => 'taxonomy'
+         * 'source' => 'category',
+         *
+         * 'population' => 'users'
+         * 'source' => array( 'editor', 'subscriber', 'author' ),
+         *
+         * 'population' => 'array'
+         * 'source' => '' // will populate with 'choices' array
+         */
+        'source' => '',
+        //'population' => 'posts',
+        //'source' => 'post',
+        /**
+         * Установите количество posts/users/taxonomies, которые будут выбраны с множественным выбором
+         * Или установите значение false, чтобы отключить эту функцию.
+         */
+        'prepopulate' => 10,
+        /**
+         * Массив с доступными вариантами выбора
+         * Используется только тогда, когда 'population' => 'array'
+         */
+        'choices' => array(
+            'choice-1' => __('Choice 1', '{domain}'),
+            'choice-2' => __('Choice 2', '{domain}'),
+            'choice-3' => __('Choice 3', '{domain}'),
+        ),
+        /**
+         * Установить количество максимальных элементов, которые могут быть выбраны
+         */
+        'limit' => 100,
+    ),
+    'option_switch' => array(
+        'type'  => 'switch',
+        'value' => 'hello',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+        'left-choice' => array(
+            'value' => 'goodbye',
+            'label' => __('Goodbye', '{domain}'),
+        ),
+        'right-choice' => array(
+            'value' => 'hello',
+            'label' => __('Hello', '{domain}'),
+        ),
+    ),
+    'option_color_picker' => array(
+        'type'  => 'color-picker',
+        'value' => '#FF0050',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        // Палитра цветов
+        'palettes' => array( '#ba474e', '#0ce0ed', '#946940' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+    ),
+    'option_rgba_color_picker' => array(
+        'type'  => 'rgba-color-picker',
+        'value' => 'rgba(245,0,0,0.5)',
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        // Палитра цветов
+        'palettes' => array( '#ba4e4e', '#0ce9ed', '#941940' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+    ),
+    'option_gradient' => array(
+        'type'  => 'gradient',
+        'value' => array(
+            'primary'   => '#0000FF',
+            'secondary' => '#FF0000',
+        ),
+        'attr'  => array( 'class' => 'custom-class', 'data-foo' => 'bar' ),
+        'label' => __('Label', '{domain}'),
+        'desc'  => __('Description', '{domain}'),
+        'help'  => __('Help tip', '{domain}'),
+    ),
 );
